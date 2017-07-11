ebe83fd65ad7f379d305cdd0c83c4e3b9af06e9c
Joachim Schirrmacher <j.schirrmacher@justso.de>
2017-07-11 22:16:43 +0200

    App shows list of mods

--- backend/index.js
+++ backend/index.js
@@ -9,8 +9,18 @@ const fs = require('fs')
 const express = require('express')
 const app = express()
 
+app.use((req, res, next) => {
+    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate')
+    res.header('Expires', '-1')
+    res.header('Pragma', 'no-cache')
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    res.charset = 'utf-8'
+    next()
+})
+
 app.get('/mods', (req, res) => {
-    fs.readdir('mods', (err, files) => res.json(files))
+    fs.readdir('mods', (err, files) => res.json({mods: files.map((file, index) => ({id: index + 1, name: file}))}))
 })
 
 app.listen(3001)
--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Row from './Row'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Cosmoteer Mod Editor</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    state = {
+        mods: []
+    }
+
+    componentWillMount() {
+        const req = new XMLHttpRequest()
+        req.onload = this.listener.bind(this)
+        req.open('get', 'http://localhost:3001/mods')
+        req.send()
+    }
+
+    listener(event) {
+        this.setState(JSON.parse(event.currentTarget.responseText))
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>Cosmoteer Mod Editor</h2>
+                </div>
+                <ul className="App-intro">
+            {this.state.mods.length
+                ? this.state.mods.map((row) => <Row key={row.id} name={row.name} />)
+                : 'Keine Mods gefunden'
+                }
+                </ul>
+            </div>
+        )
+    }
 }
 
 export default App;

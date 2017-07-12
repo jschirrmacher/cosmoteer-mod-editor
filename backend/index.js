cbdd7de1c3f51aebf04a779934508fc7e74d101d
Joachim Schirrmacher <j.schirrmacher@justso.de>
2017-07-12 17:28:57 +0200

    Removed CORS headers (not needed any more)

--- backend/index.js
+++ backend/index.js
@@ -13,8 +13,6 @@ app.use((req, res, next) => {
     res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate')
     res.header('Expires', '-1')
     res.header('Pragma', 'no-cache')
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     res.charset = 'utf-8'
     next()
 })

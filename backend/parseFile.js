ebc079a9ce1f57a53909c0493721d8524eacd591
Jasper <jasper.gerigk@gmx.net>
2017-07-12 20:22:46 +0200

    Parser runs in most cases. LINKS are not handled!

--- backend/parseFile.js
+++ backend/parseFile.js
@@ -7,7 +7,8 @@ const tokeniser = require("node-tokenizer")
 
 tokeniser.rule("whitespace", /^[\r\n\t ]+/)
 tokeniser.rule("arrayStart", /^[a-zA-Z]+\s*\[/)
-tokeniser.rule("bracket", /^[\]\{\}]/)
+tokeniser.rule("objectStart", /^[a-zA-Z]+\s*\{/)
+tokeniser.rule("bracket", /^[\[\]\{\}]/)
 tokeniser.rule("string", /^[^\r\n\[\]\{\}]+/)
 
 
@@ -15,7 +16,7 @@ exports.readFile = (fileName) => {
     const lines = tokeniser.tokenize(fs.readFileSync(fileName).toString()).map(element => element.trim()).filter(element => !element.match(/^$/))
     console.log(lines)
     let data = {}
-    let counter = 0
+    data.randomData = []
     let stack = [data]
     let active = data
     lines.forEach(line => {
@@ -29,17 +30,35 @@ exports.readFile = (fileName) => {
         //
         else{
             if(line.substr(-1) === "["){
-                active[line.substr(0, -1)] = []
-                stack.push(active[line.substr(0, -1)])
+                active[line.substr(0, line.length - 1).trim()] = []
+                stack.push(active[line.substr(0, line.length - 1).trim()])
                 active = stack[stack.length - 1]
-            } else if(line === "]"){
+            } else if(line == "["){
+                //todo: handle unammed Arrays
+                //todo: unnammed Arrays werden nicht in den Stack gepuscht, da sie nur skalars haben
+                throw "This has not been implemented! " + line
+            } else if(line === "]" || line === "}"){
                 active = stack[stack.length - 2]
                 stack.pop()
-            } else if(line == "{"){
-
+            } else if(line == "{") {
+                if (Array.isArray(active)) {
+                    active.push({})
+                    stack.push(active[active.length - 1])
+                    active = active[active.length - 1]
+                    active.randomData = []
+                } else {
+                    //todo: handle unnammed objects in obejcts (if that occurs)
+                    throw "This has not been implemented!" + line
+                }
+            } else{
+                //todo: handle links + other data
+                if(Array.isArray(active)){
+                    active.push(line)
+                } else{
+                    active.randomData.push(line)
+                }
             }
         }
-        counter++
     })
     console.log(data)
     return data;

b954be618db44e465f919bf9f6b05d0e4624f0c1
Jasper <jasper.gerigk@gmx.net>
2017-07-13 16:58:33 +0200

    Created design for mod creation

--- src/App.js
+++ src/App.js
@@ -25,12 +25,34 @@ class App extends Component {
                     <h2>Cosmoteer Mod Editor</h2>
                 </div>
                 <ul className="App-intro">
-            {this.state.mods.length
-                ? this.state.mods.map((row) => <Row key={row.id} data={row} />)
-                : 'Keine Mods gefunden'
+                {this.state.mods.length
+                    ? this.state.mods.map((row) => <Row key={row.id} data={row} />)
+                    : 'Keine Mods gefunden'
                 }
                 </ul>
+                <div className="addMod">
+                    <img className="addModImage" src="/mods/x/media/logo.png"/>
+                    <form className="addModForm">
+                        <div className="newModDiv" id="newModID">
+                            Mod ID:
+                            <input className="newModInput" type="text" name="id"/>
+                        </div>
+                        <div className="newModDiv" id="newModName">
+                            Name:
+                            <input className="newModInput" type="text" name="name"/>
+                        </div>
+                        <div className="newModDiv" id="newModAuthor">
+                            Author:
+                            <input className="newModInput" type="text" name="author"/>
+                        </div>
+                        <div className="newModDiv" id="newModVersion">
+                            Version: <input className="newModInput" type="text" name="version"/>
+                        </div>
+                        <input className="newModSubmit" type="submit" value="Create Mod"/>
+                    </form>
+                </div>
             </div>
+
         )
     }
 }

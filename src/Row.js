4ebf4d8c660a1485387eda5e3ee15ce363dd0457
Joachim Schirrmacher <j.schirrmacher@justso.de>
2017-07-13 08:20:35 +0200

    Toggle description on click

--- src/Row.js
+++ src/Row.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react'
 import './Row.css'
 
 class Row extends Component {
+    state = {active: false}
+
+    select() {
+        this.setState({active: !this.state.active})
+    }
+
     render() {
-        let description = this.props.active ? <span className="description">{this.props.data.description}</span> : ''
+        let description = this.state.active ? <span className="description">{this.props.data.description}</span> : ''
         return (
-            <li>
+            <li onClick={() => this.select()}>
                 <img src={this.props.data.logo} />
                 <span className="title">{this.props.data.title}</span>
                 <span className="author">{this.props.data.author}</span>

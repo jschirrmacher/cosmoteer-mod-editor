909570b036ef28189fef7701abc35522480b7f32
Joachim Schirrmacher <j.schirrmacher@justso.de>
2017-07-13 08:01:01 +0200

    Show logo. Added logging of accesses to server. Id is only name of mod now.

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,17 @@
 'use strict'
 
 const fs = require('fs')
+const path = require('path')
 const express = require('express')
 const parser = require("./parseFile")
 const glob = require("glob")
 
 const app = express()
+
+app.use((req, res, next) => {
+    console.log(req.method + ' ' + req.path)
+    next()
+})
 
 app.use((req, res, next) => {
     res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate')
@@ -24,15 +30,21 @@ app.get('/mods', (req, res) => {
     glob('mods/*/mod.txt', (err, files) => { console.log(files); res.json({
         mods: files.map(file => {
             let data = parser.readFile(file)
+            let id = file.replace(/mods\/(.*?)\/mod.txt/, '$1')
             return {
-                id: file,
+                id: id,
                 title: data.Name,
                 author: data.Author,
                 version: data.Version,
-                description: data.Description
+                description: data.Description,
+                logo: '/mods/' + id + '/media/' + data.Logo
             }
         })
     })})
+})
+
+app.get('/mods/:mod/media/:file', (req, res) => {
+    res.sendFile(path.join(__dirname, 'mods', req.params.mod, req.params.file))
 })
 
 app.listen(3001)

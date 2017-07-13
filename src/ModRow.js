133d5da58fb988739a751436b69d2d96efcb3596
Joachim Schirrmacher <j.schirrmacher@justso.de>
2017-07-13 18:54:04 +0200

    Editable input fields (not yet writing to the backend)

--- src/ModRow.js
+++ src/ModRow.js
@@ -2,10 +2,26 @@ import React, { Component } from 'react'
 import './ModRow.css'
 
 class ModRow extends Component {
-    state = {active: false}
+    state = {
+        active: false,
+        value: {}
+    }
+
+    componentWillMount() {
+        this.setState({ value: this.props.data })
+    }
 
     select() {
-        this.setState({active: !this.state.active})
+        this.setState({ active: true })
+    }
+
+    changed(change) {
+        this.setState(state => {
+            state.value[change.name] = change.value
+            return {
+                value: state.value
+            }
+        })
     }
 
     render() {
@@ -13,16 +29,14 @@ class ModRow extends Component {
             return {__html: data.description}
         }
 
-        let description = this.state.active ?
-            <span className="description" dangerouslySetInnerHTML={getDescription(this.props.data)}></span> :
-            ''
         return (
             <li onClick={() => this.select()}>
-                <img src={this.props.data.logo} />
-                <span className="title">{this.props.data.title}</span>
-                <span className="author">{this.props.data.author}</span>
-                <span className="version">{this.props.data.version}</span>
-                {description}
+                <img src={this.props.data.logo} alt="Mod Logo" />
+                <input type="text" name="title" value={this.props.data.title} onChange={e => this.changed(e.target)} />
+                <input type="text" name="author" value={this.props.data.author} onChange={e => this.changed(e.target)} />
+                <input type="text" name="version" value={this.props.data.version} onChange={e => this.changed(e.target)} />
+                <span className="description" contentEditable onInput={e => this.changed(e.target)}
+                    dangerouslySetInnerHTML={getDescription(this.props.data)}></span>
             </li>
         )
     }

d60183b62405dbf11c7c6198c87061de4783adfa
Joachim Schirrmacher <j.schirrmacher@justso.de>
2017-07-13 10:33:13 +0200

    Refactoring

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,8 @@
  */
 'use strict'
 
-const fs = require('fs')
-const path = require('path')
 const express = require('express')
-const parser = require("./parseFile")
-const glob = require("glob")
+const modHandler = require('./modHandler')
 
 const app = express()
 
@@ -26,31 +23,8 @@ app.use((req, res, next) => {
     next()
 })
 
-app.get('/mods', (req, res) => {
-    glob('mods/*/mod.txt', (err, files) => { console.log(files); res.json({
-        mods: files.map(file => {
-            let data = parser.readFile(file)
-            let id = file.replace(/mods\/(.*?)\/mod.txt/, '$1')
-            return {
-                id: id,
-                title: data.Name,
-                author: data.Author,
-                version: data.Version,
-                description: data.Description,
-                logo: '/mods/' + id + '/media/' + data.Logo
-            }
-        })
-    })})
-})
-
-app.get('/mods/:mod/media/:file', (req, res) => {
-    var file = path.join(__dirname, 'mods', req.params.mod, req.params.file)
-    if (fs.existsSync(file)) {
-        res.sendFile(file)
-    } else {
-        res.sendFile(path.join(__dirname, 'plug.png'))
-    }
-})
+app.get('/mods', modHandler.listMods)
+app.get('/mods/:mod/media/:file', modHandler.getMediaFile)
 
 app.listen(3001)
 console.log('Server running')
---
+++ backend/modHandler.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+const parser = require("./parseFile")
+const glob = require('glob')
+
+module.exports = {
+    listMods: (req, res) => {
+        glob('mods/*/mod.txt', (err, files) => { console.log(files); res.json({
+            mods: files.map(file => {
+                let data = parser.readFile(file)
+                let id = file.replace(/mods\/(.*?)\/mod.txt/, '$1')
+                return {
+                    id: id,
+                    title: data.Name,
+                    author: data.Author,
+                    version: data.Version,
+                    description: data.Description,
+                    logo: '/mods/' + id + '/media/' + data.Logo
+                }
+            })
+        })})
+    },
+    getMediaFile: (req, res) => {
+        var file = path.join(__dirname, 'mods', req.params.mod, req.params.file)
+        if (fs.existsSync(file)) {
+            res.sendFile(file)
+        } else {
+            res.sendFile(path.join(__dirname, 'plug.png'))
+        }
+    }
+}

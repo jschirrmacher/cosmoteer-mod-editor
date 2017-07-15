537139adc8811f7c52fbf87b1643b69bc4f0f535
Joachim Schirrmacher <j.schirrmacher@justso.de>
2017-07-15 22:57:48 +0200

    Added frontend tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+/*global it, beforeAll*/
+import React from 'react'           // eslint-disable-line no-unused-vars
+import { shallow } from 'enzyme'    // eslint-disable-line no-unused-vars
+import { expect } from 'chai'
+
+import App from './App'             // eslint-disable-line no-unused-vars
+import Row from './ModRow'       // eslint-disable-line no-unused-vars
+
 global.fetch = require('jest-fetch-mock')
 
+beforeAll(done => {
+    fetch.mockResponse(JSON.stringify({mods: []}))
+    done()
+})
+
 it('renders without crashing', () => {
-    const div = document.createElement('div')
-    fetch.mockResponse(JSON.stringify({
-        mods: [{id: 1, name: 'test'}]
-    }))
-    ReactDOM.render(<App />, div)
+    shallow(<App />)
+})
+
+it('renders a message when there are no mods', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find('.App-intro').text()).to.equal('No Mods found')
+})
+
+it('renders a form', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find('form')).to.have.length(1)
+})
+
+it('renders a list of mods', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find(Row)).to.have.length(0)
+    wrapper.setState({mods: [{id: 1, name: 'test'}, {id: 2, name: 'test 2'}]})
+    expect(wrapper.find(Row)).to.have.length(2)
 })

aae2d70d4275bd8a3ea839a3f50dbb9a3204b49c
Jasper <jasper.gerigk@gmx.net>
2017-07-15 12:52:23 +0200

    Chnaged rules

--- backend/rules.js
+++ backend/rules.js
@@ -1,10 +1,10 @@
 module.exports = [
-    {type: "comment", regex: /^\s*\/\/[^\n]*$/},
-    {type: "definition", regex: /^\s*\w+\s*=(?:[^\n]+\\\n)*[^\n]+$/},
-    {type: "arrayStart", regex: /^\s*\w*\s*\[$/},
-    {type: "arrayEnd", regex: /^\s*\]$/},
-    {type: "objectStart", regex: /^.*\n?\{$/},
-    {type: "objectEnd", regex: /^\s*\}$/},
+    {type: "comment", regex: /^\s*\/\/[^\n]*/},
+    {type: "definition", regex: /^\s*(\w+)\s*=((?:[^\n]+\\\n)*)[^\n]+/},
+    {type: "arrayStart", regex: /^\s*\w*\s*\[/},
+    {type: "arrayEnd", regex: /^\s*\]/},
+    {type: "objectStart", regex: /^.*\n?\{/},
+    {type: "objectEnd", regex: /^\s*\}/},
     {type: "line", regex: /^.+$/},
-    {type: "emptyLine", regex: /^\n*$/}
+    {type: "emptyLine", regex: /^\n*/}
 ]

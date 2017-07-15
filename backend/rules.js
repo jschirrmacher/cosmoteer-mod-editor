7595e21a3a7a291689f86b92a244ecb2018df9db
Joachim Schirrmacher <j.schirrmacher@justso.de>
2017-07-15 13:05:33 +0200

    Added tests for simple arrays and objects

--- backend/rules.js
+++ backend/rules.js
@@ -1,9 +1,9 @@
 module.exports = [
     {type: "comment", regex: /^\s*\/\/[^\n]*/},
     {type: "definition", regex: /^\s*(\w+)\s*=((?:[^\n]+\\\n)*)[^\n]+/},
-    {type: "arrayStart", regex: /^\s*\w*\s*\[/},
+    {type: "arrayStart", regex: /^\s*(\w*)\s*\[/},
     {type: "arrayEnd", regex: /^\s*\]/},
-    {type: "objectStart", regex: /^.*\n?\{/},
+    {type: "objectStart", regex: /^\s*(\w*)\s*\{/},
     {type: "objectEnd", regex: /^\s*\}/},
     {type: "line", regex: /^.+$/},
     {type: "emptyLine", regex: /^\n*/}
--- backend/tokeniser.test.js
+++ backend/tokeniser.test.js
@@ -14,8 +14,31 @@ describe('Tokeniser', () => {
 
     it('should recognise definitions')
     it('should recognise definitions with continuation lines')
-    it('should recognise arrays')
-    it('should recognise objects')
+
+    it('should recognise arrays', done => {
+        let result = tokeniser('abc [\n\tx = 1\n]', rules)
+        result.should.be.an.array
+        result.length.should.equal(3)
+        result[0].type.should.equal('arrayStart')
+        result[0].matches[0].should.equal('abc [')
+        result[0].matches[1].should.equal('abc')
+        result[2].type.should.equal('arrayEnd')
+        result[2].matches[0].trim().should.equal(']')
+        done()
+    })
+
+    it('should recognise objects', done => {
+        let result = tokeniser('abc {\n\tx = 1\n}', rules)
+        result.should.be.an.array
+        result.length.should.equal(3)
+        result[0].type.should.equal('objectStart')
+        result[0].matches[0].should.equal('abc {')
+        result[0].matches[1].should.equal('abc')
+        result[2].type.should.equal('objectEnd')
+        result[2].matches[0].trim().should.equal('}')
+        done()
+    })
+
     it('should recognise objects in an array')
     it('should recognise arrays in an array')
     it('should recognise literals in an array')

63008ec647475e203c17d4e9c1e187f8234e8f61
Joachim Schirrmacher <j.schirrmacher@justso.de>
2017-07-16 20:46:59 +0200

    Fixed a problem with always changing test files

--- backend/modHandler.test.js
+++ backend/modHandler.test.js
@@ -1,6 +1,7 @@
 /**
  * Created by Jasper on 15.07.2017.
  */
+/*eslint-env mocha*/
 const should = require('should') // eslint-disable-line no-unused-vars
 const modHandler = require('./modHandler')
 const parser = require('./parseFile')
@@ -8,21 +9,21 @@ const path = require('path')
 
 
 describe('Mod Handler', () => {
-    it("Read Mod File reacts correctly to improper file", done => {
-        let result = modHandler.readModFile(".Failure", "Test Files", true)
+    it('Read Mod File reacts correctly to improper file', done => {
+        let result = modHandler.readModFile('.Failure', 'Test Files', true)
         //noinspection BadExpressionStatementJS
         result.should.false
         done()
     })
 })
 
-describe("Parser and Mod Handler File Handling", () => {
-    it("Parser creates and writes file safely", done =>{
+describe('Parser and Mod Handler File Handling', () => {
+    it('Parser creates and writes file safely', done =>{
         //Save file with newline
-        parser.writeToFile(parser.fromObjectToText({description: "this may \n fail"}), path.join(__dirname, "Test Files",".Safe", "mod.txt"))
+        parser.writeToFile(parser.fromObjectToText({description: 'this\n may \n fail'}), path.join(__dirname, 'Test Files', '.Safe', 'mod.txt'))
         //Load
-        let result = modHandler.readModFile(".Safe", "Test Files", true)
-        result.description.should.equal("this may \\n fail")
+        let result = modHandler.readModFile('.Safe', 'Test Files', true)
+        result.description.should.equal('this\\n may \\n fail')
         done()
     })
 })
